eb3755d96f275742bd6a683e23232a03622679d9
Ricardo Georgel <rickygeorgel@yahoo.com.br>
2020-04-04 17:51:56 -0300

    Updates

---
+++ precache-manifest.1d91689dd2ac0ca214eb5db18d50dec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8817c7c8363e7363a7c45be1146a95",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "121b84adc3d524e3b7c8",
+    "url": "/monsters-rolodex/static/css/main.2b06b07c.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "121b84adc3d524e3b7c8",
+    "url": "/monsters-rolodex/static/js/main.dbed9212.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
